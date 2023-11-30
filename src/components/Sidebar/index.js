814ec9f9af65f9f40750ebf6dda356a7fefc2852
JoshuaHamann <joshwashku@yahoo.com>
2023-11-30 07:12:55 -0600

    added css to ref icon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -33,6 +33,24 @@ const Sidebar = () => (
                     <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
                 </a>
             </li>
+            <li>
+                <a 
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://github.com/Joshwashku1'
+                >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            {/* <li>
+                <a 
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://www.linkedin.com/in/joshua-hamann-b83110205/'
+                >
+                    <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+                </a>
+            </li> */}
         </ul>
     </div>
 )
