68939651495476bfc4001580a7dfab97573f73c0
JoshuaHamann <joshwashku@yahoo.com>
2023-12-01 12:24:22 -0600

    completed pacman animation and set it also to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,8 @@ import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo';
+import Loader from 'react-loaders';
+
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
@@ -16,6 +18,7 @@ const Home = () => {
     }, [])
     
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
@@ -38,6 +41,8 @@ const Home = () => {
             </div>
             <Logo />
         </div>
+        <Loader type='pacman' />
+        </>
     );
 }
 
